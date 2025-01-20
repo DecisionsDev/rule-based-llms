c8885478c520e459e6894fa5a302a2aa0eab6beb
laurent grateau <laurent.grateau@fr.ibm.com>
2025-01-20 15:33:20 +0100

    Set debug

--- rule-agent/tests/test.py
+++ rule-agent/tests/test.py
@@ -2,7 +2,7 @@ import os,sys
 
 import sys
 sys.path.append("..")
-
+from langchain.globals import set_debug
 import prompts
 from langchain.llms import Ollama
 from langchain_core.prompts import ChatPromptTemplate
@@ -12,7 +12,7 @@ llm = Ollama(base_url="http://localhost:11434",
                 temperature=0)
 import json
 from pprint import pprint
-
+set_debug(True)
 with open('result.json') as json_data:
     result = json.load(json_data)
 
